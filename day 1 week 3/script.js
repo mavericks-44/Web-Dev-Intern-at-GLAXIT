094829890a6b8591976f981444310ba8cdd38a80
Anees Ul Haq <aneesulhaq935@gmail.com>
2025-04-30 14:49:33 +0500

    java script basic

---
+++ day 1 week 3/script.js
@@ -0,0 +1,36 @@
+
+// JavaScript Introduction Example
+
+// 1. JavaScript Output Examples
+
+// Using console.log to output to the console
+console.log("Hello, world!");
+
+// Using alert to show an alert box with a message
+alert("Hello, world!");
+
+// Using document.write to display content in the HTML document
+document.write("Hello, world!");
+
+// Using innerHTML to change content of an HTML element with id "demo"
+document.getElementById("demo").innerHTML = "Hello, world!";
+
+// 2. JavaScript Statement Example
+let x = 5;  // This is an assignment statement
+
+// 3. JavaScript Syntax Example
+if (x > 0) {
+    console.log("x is greater than 0");
+} else {
+    console.log("x is not greater than 0");
+}
+
+// 4. JavaScript Variables Example
+let name = "Ahmed";  
+const birthYear = 1998;  // Using 'const' to declare a constant
+var age = 30;  
+
+// Output the values of variables
+console.log("Name: " + name);  // Output: Name: John
+console.log("Age: " + age);    // Output: Age: 30
+console.log("Birth Year: " + birthYear);  // Output: Birth Year: 1998
